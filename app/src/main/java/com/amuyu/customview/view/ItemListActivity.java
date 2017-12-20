00a9f4d7aeebc0197bee1121be5bd3f207134999
amuyu <hwajoong.song@gmail.com>
2017-12-20 17:06:18 +0900

    Add background to list item

--- app/src/main/java/com/amuyu/customview/view/ItemListActivity.java
+++ app/src/main/java/com/amuyu/customview/view/ItemListActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -62,6 +63,7 @@ public class ItemListActivity extends AppCompatActivity {
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, ViewList.ITEMS, mTwoPane));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
     public static class SimpleItemRecyclerViewAdapter
